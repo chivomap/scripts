189fec423a3e880e2f1fc1c9f4f07e98472326f1
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-10-31 08:17:14 -0600

    feat: :sparkles: update initial data

--- data/sql/initial_data.sql
+++ data/sql/initial_data.sql
@@ -1,1 +1,5 @@
 INSERT INTO type_buses (id, name) VALUES (1, 'AutoBus'), (2, 'MicroBus');
+INSERT INTO subtype_buses (id, name) VALUES (1, 'Interdepartamental'), (2, 'Urbano'), (3, 'Interurbano');
+
+INSERT INTO direction_routes (id, name) VALUES (1, 'Ida'), (2, 'Vuelta');
+
